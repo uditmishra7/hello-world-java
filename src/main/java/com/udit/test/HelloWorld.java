2ea841430187325a9ffa45330914ccce74efc009
Udit Mishra <mishra.uditnarayan1@gmail.com>
2022-12-25 15:57:19 +0530

    extracted print to a new method

--- src/main/java/com/udit/test/HelloWorld.java
+++ src/main/java/com/udit/test/HelloWorld.java
@@ -3,6 +3,10 @@ package com.udit.test;
 public class HelloWorld {
 
     public static void main(String[] args) {
+        printHello();
+    }
+
+    private static void printHello() {
         System.out.println("This is Main Method.....");
     }
 }
