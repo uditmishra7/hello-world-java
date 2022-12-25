28c9986071283c346a656887b5bce095d1cf9cf1
Udit Mishra <mishra.uditnarayan1@gmail.com>
2022-12-25 15:59:41 +0530

    Added new method to print hello world

--- src/main/java/com/udit/test/HelloWorld.java
+++ src/main/java/com/udit/test/HelloWorld.java
@@ -4,9 +4,14 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         printHello();
+        printHelloWorld();
     }
 
     private static void printHello() {
         System.out.println("This is Main Method.....");
+    }
+
+    private static void printHelloWorld() {
+        System.out.println("This is Hello World...");
     }
 }
